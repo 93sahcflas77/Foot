7505f95c5b2e01cc0a86eb80a3da1e2296527741
93sahcflas77 <139437921+93sahcflas77@users.noreply.github.com>
2023-12-27 19:35:32 +0000

    update

--- script.js
+++ script.js
@@ -9,3 +9,95 @@ a.addEventListener('click',(e) => {
     b.classList.toggle('toggle')
 
 });
+
+
+
+const product = [
+    {
+        Product_name: "Organic Tomato",
+        rate: "$15.30",
+        Discount: "$12.30",
+        url: "image/10.jpg"
+    }
+    ,
+    {
+        Product_name: "Organic Banana",
+        rate: "$10.30",
+        Discount: "$8.12",
+        url: "image/9.jpg"
+    }
+    ,
+    {
+        Product_name: "Organic Grapes",
+        rate: "$80.50",
+        Discount: "$40.65",
+        url: "image/8.jpg"
+    },
+    {
+        Product_name: "Organic Lemon",
+        rate: "$102.80",
+        Discount: "$40.10",
+        url: "image/5.jpg"
+    },
+    {
+        Product_name: "Organic Broccoli",
+        rate: "$55.10",
+        Discount: "$30.20",
+        url: "image/6.jpg"
+    },
+    {
+        Product_name: "Organic Potato",
+        rate: "$12.00",
+        Discount: "$6.00",
+        url: "image/7.jpg"
+    },
+    {
+        Product_name: "Organic",
+        rate: "$10.30",
+        Discount: "$8.12",
+        url: ""
+    }
+];
+
+
+// if(product[1].Product_name == "Aalu"){
+//     console.log(`This is ${product[1].Product_name} product name`)
+// }else{
+//     console.log("error");
+// }
+
+// console.log(product[1].Product_name);
+
+
+const product_container = document.querySelector(".box-container");
+
+// console.log(product_container);
+
+ product.forEach(element => {
+    product_container.innerHTML +=  `
+    <div class="box">
+                <div>
+                    <img src="${element.url}" alt="">
+                </div>
+                <h3>${element.Product_name}</h3>
+                <ul class="star">
+                    <li><i class="fa-solid fa-star"></i></li>
+                    <li><i class="fa-solid fa-star"></i></li>
+                    <li><i class="fa-solid fa-star"></i></li>
+                    <li><i class="fa-solid fa-star"></i></li>
+                    <li><i class="fa-solid fa-star-half"></i></li>
+                </ul>
+                <div class="price">
+                    <span>${element.rate}</span> <span>${element.Discount}</span>
+                </div>
+                <ul class="icon">
+                    <li><i class="fa-solid fa-cart-shopping"></i></li>
+                    <li><i class="fa-solid fa-eye"></i></li>
+                    <li><i class="fa-solid fa-heart"></i></li>
+                    <li><i class="fa-solid fa-share"></i></li>
+                </ul>
+            </div>
+    `
+
+    console.log(element);
+ });   
